5fb68ac57c4589d8e6ba261123ac65ac02f23713
Oragwel <tidingstechnologies@gmail.com>
2025-04-10 03:34:57 +0300

    Implement middle_day function to return the weekday of the year's middle day using chrono

---
+++ middle_day/src/lib.rs
@@ -0,0 +1,13 @@
+use chrono::{Datelike, NaiveDate, Weekday as wd};
+
+pub fn middle_day(year: i32) -> Option<wd> {
+    let is_leap = NaiveDate::from_ymd_opt(year, 2, 29).is_some();
+    let num_days = if is_leap { 366 } else { 365 };
+
+    if num_days % 2 == 0 {
+        return None;
+    }
+
+    let middle = num_days / 2 + 1;
+    NaiveDate::from_yo_opt(year, middle as u32).map(|d| d.weekday())
+}
