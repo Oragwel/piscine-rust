ce19233f7cb3ceb94bae2097facdf5313f44261c
Oragwel <tidingstechnologies@gmail.com>
2025-04-13 01:49:37 +0300

    implement symmetric diamond creation based on input character

--- diamond_creation/src/lib.rs
+++ diamond_creation/src/lib.rs
@@ -1,30 +1,25 @@
 pub fn get_diamond(c: char) -> Vec<String> {
-    let n = (c as u8 - b'A') as usize;
-    let mut diamond = Vec::new();
-
-    for i in 0..=n {
-        let letter = (b'A' + i as u8) as char;
-        let outer_spaces = n - i;
-        let inner_spaces = if i == 0 { 0 } else { 2 * i - 1 };
-        let line = if i == 0 {
-            format!("{:width$}", letter, width = n + 1)
-        } else {
-            format!(
-                "{:outer$}{}{:inner$}{}",
-                "",
-                letter,
-                "",
-                letter,
-                outer = outer_spaces,
-                inner = inner_spaces
-            )
-        };
-        diamond.push(line);
+    let mut result = Vec::new();
+    let n = (c as u8 - b'A' + 1) as usize;
+    for i in 0..n {
+        let spaces = " ".repeat(n - i - 1);
+        let mut s = format!("{}{}", spaces, (b'A' + i as u8) as char);
+        if i > 0 {
+            let inner_spaces = " ".repeat(2 * i - 1);
+            s = format!("{}{}{}", s, inner_spaces, (b'A' + i as u8) as char);
+        }
+        s = format!("{}{}", s, spaces);
+        result.push(s);
     }
-
-    for i in (0..n).rev() {
-        diamond.push(diamond[i].clone());
+    for i in (0..n - 1).rev() {
+        let spaces = " ".repeat(n - i - 1);
+        let mut s = format!("{}{}", spaces, (b'A' + i as u8) as char);
+        if i > 0 {
+            let inner_spaces = " ".repeat(2 * i - 1);
+            s = format!("{}{}{}", s, inner_spaces, (b'A' + i as u8) as char);
+        }
+        s = format!("{}{}", s, spaces);
+        result.push(s);
     }
-
-    diamond
+    result
 }
