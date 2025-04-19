f181534eac065efa0881f4b99a86ffaccd5cdbdd
Oragwel <tidingstechnologies@gmail.com>
2025-04-19 22:43:16 +0300

    Implement Vector struct with dot product function and required traits (Debug, Clone, Eq, PartialEq)

--- lalgebra_vector/src/lib.rs
+++ lalgebra_vector/src/lib.rs
@@ -1,42 +1,29 @@
-use std::fmt::Debug;
-use std::ops::{Add, Mul};
+use lalgebra_scalar::Scalar;
 
-pub trait Scalar:
-    Add<Output = Self>
-    + Mul<Output = Self>
-    + Copy
-    + Clone
-    + Debug
-    + PartialEq
-    + Eq
-{
-    fn zero() -> Self;
-    fn one() -> Self;
-}
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct Vector<T: Scalar<Item = T>>(pub Vec<T>);
 
-impl Scalar for i64 {
-    fn zero() -> Self {
-        0
-    }
+use std::ops::Add;
 
-    fn one() -> Self {
-        1
-    }
-}
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub struct Vector<T: Scalar>(pub Vec<T>);
-
-impl<T: Scalar> Add for Vector<T> {
-    type Output = Self;
-
+impl<T: Scalar<Item = T> + Add<Output = T>> Add<Self> for Vector<T> {
+    type Output = Option<Self>;
     fn add(self, other: Self) -> Self::Output {
-        let data = self.0.iter().zip(other.0.iter()).map(|(a, b)| *a + *b).collect();
-        Vector(data)
+        if self.0.len() != other.0.len() {
+            return None;
+        }
+
+        let result: Vector<T> = Vector(
+            self.0
+                .iter()
+                .zip(other.0.iter())
+                .map(|(x, y)| x.clone() + y.clone())
+                .collect(),
+        );
+        Some(result)
     }
 }
 
-impl<T: Scalar> Vector<T> {
+impl<T: Scalar<Item = T> + std::iter::Sum<<T as std::ops::Mul>::Output>> Vector<T> {
     pub fn new() -> Self {
         Vector(Vec::new())
     }
@@ -45,10 +32,30 @@ impl<T: Scalar> Vector<T> {
         if self.0.len() != other.0.len() {
             return None;
         }
-        let mut result = T::zero();
-        for (a, b) in self.0.iter().zip(other.0.iter()) {
-            result = result + (*a * *b);
-        }
+        let result = self
+            .0
+            .iter()
+            .zip(other.0.iter())
+            .map(|(x, y)| x.clone() * y.clone())
+            .sum();
+
         Some(result)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn dot_product() {
+        let vector_1: Vector<i64> = Vector(vec![1, 3, -5]);
+        let vector_2: Vector<i64> = Vector(vec![4, -2, -1]);
+        let expected: i64 = 3;
+        assert_eq!(vector_1.dot(&vector_2), Some(expected));
+
+        let vector_1: Vector<i64> = Vector(vec![1, 3, -5]);
+        let vector_2: Vector<i64> = Vector(vec![4, -2]);
+        assert_eq!(vector_1.dot(&vector_2), None);
     }
 }
