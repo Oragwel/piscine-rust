eaac4d08e40f96437452cd669a8de93029cbecdd
Oragwel <tidingstechnologies@gmail.com>
2025-04-24 05:44:48 +0300

    Implement Matrix struct with basic constructors (new, zero, identity)

--- matrix/src/lib.rs
+++ matrix/src/lib.rs
@@ -1,24 +1,66 @@
 use lalgebra_scalar::Scalar;
+mod mult;
+mod ops;
 
+#[derive(Debug, Eq, PartialEq, Clone)]
 pub struct Matrix<T>(pub Vec<Vec<T>>);
 
 impl<T: Scalar<Item = T>> Matrix<T> {
-    /// Creates a new 1x1 matrix with T::zero()
     pub fn new() -> Matrix<T> {
-        Matrix(vec![vec![T::zero()]])
+        Matrix(vec![Vec::new()])
     }
-
-    /// Creates a matrix filled with zeros of size row x col
+    // It returns the zero matrix of the size given by the row and
+    // column parameters
     pub fn zero(row: usize, col: usize) -> Matrix<T> {
-        Matrix(vec![vec![T::zero(); col]; row])
+        let mut matrix = Matrix(Vec::new());
+        for _ in 0..row {
+            matrix.0.push(vec![T::zero(); col]);
+        }
+        matrix
     }
 
-    /// Creates an identity matrix of size n x n
     pub fn identity(n: usize) -> Matrix<T> {
-        let mut matrix = vec![vec![T::zero(); n]; n];
-        for i in 0..n {
-            matrix[i][i] = T::one();
+        let mut matrix = Matrix::new();
+        for y in 0..n {
+            if y > 0 {
+                matrix.0.push(Vec::new());
+            }
+            for x in 0..n {
+                if y == x {
+                    matrix.0[y].push(T::one());
+                } else {
+                    matrix.0[y].push(T::zero());
+                }
+            }
         }
-        Matrix(matrix)
+        matrix
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn zero_property() {
+        let matrix: Matrix<u32> = Matrix::zero(3, 4);
+        let expected: Matrix<u32> =
+            Matrix(vec![vec![0, 0, 0, 0], vec![0, 0, 0, 0], vec![0, 0, 0, 0]]);
+        assert_eq!(matrix, expected);
+
+        let matrix: Matrix<u32> = Matrix::zero(2, 2);
+        let expected: Matrix<u32> = Matrix(vec![vec![0, 0], vec![0, 0]]);
+        assert_eq!(matrix, expected);
+    }
+
+    #[test]
+    fn identy_matrix() {
+        let matrix: Matrix<u32> = Matrix::identity(2);
+        let expected: Matrix<u32> = Matrix(vec![vec![1, 0], vec![0, 1]]);
+        assert_eq!(matrix, expected);
+
+        let matrix: Matrix<u32> = Matrix::identity(3);
+        let expected: Matrix<u32> = Matrix(vec![vec![1, 0, 0], vec![0, 1, 0], vec![0, 0, 1]]);
+        assert_eq!(matrix, expected);
     }
 }
